003c0ea346c1bcbb6b8a6c60035800e48c57b4a8
GiuliaR11 <radugiulia@yahoo.com>
2024-04-09 16:15:12 +0300

    #1 - update: navbar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,21 @@
-import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink } from "flowbite-react";
-import Link from "next/link";
-
 export default function Header() {
   return (
     <>
-      <Navbar fluid rounded>
-        <NavbarBrand as={Link} href="/" className="pl-16">
-          <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1712275200&semt=ais" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Brand</span>
-        </NavbarBrand>
-        <NavbarCollapse className="pr-16">
-          <NavbarLink href="#">
-            <div className="hover:text-blue-500 text-slate-400 flex flex-col items-center">
-              <svg className="w-5 h-5 mr-1 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path fill-rule="evenodd" d="M4 4a1 1 0 0 1 1-1h1.5a1 1 0 0 1 .979.796L7.939 6H19a1 1 0 0 1 .979 1.204l-1.25 6a1 1 0 0 1-.979.796H9.605l.208 1H17a3 3 0 1 1-2.83 2h-2.34a3 3 0 1 1-4.009-1.76L5.686 5H5a1 1 0 0 1-1-1Z" clip-rule="evenodd"/>
-              </svg>
-              <span className="text-xs">Cosul meu</span>
-            </div>
-          </NavbarLink>
-        </NavbarCollapse>
-      </Navbar>
-      <div className="bg-white justify-end items-center flex px-2 py-2.5 border border-slate-160 cursor-pointer pr-20">
-        <Link href="/about">Contact</Link>
-      </div>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 flex justify-center">
+        <div className="flex items-center justify-between pt-4 pb-4 w-9/12 pr-4">
+          <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1712275200&semt=ais" className="h-8" alt="Flowbite Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">P.A.P.S Brazdare</span>
+          </a>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
