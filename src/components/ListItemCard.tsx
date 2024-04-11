6e5f11e4d8b7a0e168cfbc34ac6cbf19a0c05b24
cezarmocanu <cezarmocanu2014@gmail.com>
2024-04-11 08:33:30 +0300

    PAP-3: introduced clsx, formatting, md design

--- src/components/ListItemCard.tsx
+++ src/components/ListItemCard.tsx
@@ -1,16 +1,19 @@
+"use client";
 
-'use client';
-import {Button} from '@/components/boilerplate/Button';
-import Image from 'next/image'
-import { ItemAddedPreviewModal } from './ItemAddedPreviewModal';
+import Image from "next/image";
+import { ItemAddedPreviewModal } from "./ItemAddedPreviewModal";
+import clsx from "clsx";
 
 export default function ListItemCard({ className }: any) {
   return (
-    <div className={
-      `border border-slate-160
-      flex p-4 bg-white
-      ${className}`
-    }>
+    <div
+      className={clsx(
+        "p-4 bg-white border border-slate-160 gap-4 flex",
+        "flex-col items-center",
+        "lg:flex-row",
+        className
+      )}
+    >
       <Image
         src="https://www.dpat.ro/magazin/admin/uploads/camasa-motor--ford-768.jpg"
         width={300}
@@ -19,23 +22,21 @@ export default function ListItemCard({ className }: any) {
         className="rounded-lg"
       />
 
-      <div className="flex flex-col w-full pl-4 gap-4">
+      <div className="flex flex-col w-full gap-4">
         <div className="flex w-full justify-between">
-          <div className='flex flex-col gap-1'>
-            <span className="text-lg font-normal">Camasa motor Ford 35-121</span>
-            <span className="text-xl font-semibold">
-              177.00 RON
+          <div className="flex flex-col gap-1">
+            <span className="text-lg font-normal">
+              Camasa motor Ford 35-121
             </span>
+            <span className="text-xl font-semibold">177.00 RON</span>
           </div>
-          <ItemAddedPreviewModal/>
+          <ItemAddedPreviewModal />
         </div>
         <span>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua
-          </span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </span>
       </div>
     </div>
   );
 }
-
-
